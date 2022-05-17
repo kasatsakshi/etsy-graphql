4951fa0c06033894ebebab692ea837901de29665
kasatsakshi <kasat.sakshi94@gmail.com>
2022-05-17 13:41:23 -0700

    Using S3 to upload images

--- etsyback/controllers/upload.js
+++ etsyback/controllers/upload.js
@@ -1,39 +1,62 @@
 import path from 'path';
 import multer from 'multer';
 import fs from 'fs';
+import formidable from 'formidable';
+import AWS from 'aws-sdk';
 import { updateOneEntity } from '../models';
 import Shop from '../models/shop';
 
-const storage = multer.diskStorage({
-  destination(req, file, callback) {
-    const folder = './public/';
-    if (!fs.existsSync(folder)) {
-      fs.mkdirSync(folder, { recursive: true });
-    }
-    callback(null, folder);
-  },
-  filename(req, file, cb) {
-    cb(null, `image-${Date.now()}${path.extname(file.originalname)}`);
-  },
+const BUCKET_NAME = 'cmpe273sakshi';
+const s3 = new AWS.S3({
+  accessKeyId: 'AKIA3UMIWUHMD2YBFSTL',
+  secretAccessKey: '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',
 });
 
-const uploadImage = multer({
-  storage,
-  limits: { fileSize: 2000000 }, // 2 MB
-}).single('myImage');
-
 export default async function upload(req, res) {
-  uploadImage(req, res, async (err) => {
+  const form = new formidable.IncomingForm();
+  form.multiples = true;
+  form.maxFileSize = 50 * 1024 * 1024; // 5MB
+
+  let avatarUrl = '';
+
+  form.parse(req, async (err, fields, files) => {
     if (err) {
       console.log(err);
-      return res.status(400).json('Error in uploading file');
+      res.status(400).json({
+        message: 'Unable to parse Input',
+      });
     }
 
-    if (req.body.type === 'shop') {
-      await updateOneEntity(Shop, { _id: req.body.id }, { avatarUrl: req.file.path });
-    }
+    if (files.myImage) {
+      const tempFilePath = files.myImage.filepath;
+      const fileContent = fs.readFileSync(tempFilePath);
+      const fileName = `image-${Date.now()}${path.extname(files.myImage.originalFilename)}`;
 
-    console.log('Image Uploaded Successfully!');
-    return res.status(200).json(req.file.path);
+      // Setting up S3 upload parameters
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: fileName, // File name you want to save as in S3
+        Body: fileContent,
+      };
+
+      // Uploading files to the bucket
+      const data = await s3.upload(params).promise();
+      console.log(`User image uploaded successfully. ${data.Location}`);
+      avatarUrl = data.Location;
+
+      if (req.body.type === 'shop') {
+        await updateOneEntity(Shop, { _id: req.body.id }, { avatarUrl: req.file.path });
+      }
+
+      const {
+        shopId,
+        type,
+      } = fields;
+
+      if (type === 'shop') {
+        await updateOneEntity(Shop, { _id: shopId }, { avatarUrl });
+      }
+    }
+    return res.status(200).json(avatarUrl);
   });
 }
